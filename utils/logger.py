b033cdf52a973418892c5d703ac8ada5b5d492f9
Martín Carusso <martincarusso@gmail.com>
2020-09-14 15:21:08 -0300

    some more compatibility with tf logger

--- utils/logger.py
+++ utils/logger.py
@@ -17,8 +17,11 @@ class Logger(object):
 
     def scalar_summary(self, tag, value, step):
         """Log a scalar variable."""
-        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
-        self.writer.add_summary(summary, step)
+        # summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
+        # self.writer.add_summary(summary, step)
+        with self.writer.as_default():
+            tf.summary.scalar(tag, value, step = step)
+            self.writer.flush()
 
     def image_summary(self, tag, images, step):
         """Log a list of images."""
